e5f7d4d0c476e9df9fa5482d743311f0b9b088d1
BiscitAlt <thitiputt.pe@gmail.com>
2026-01-06 20:18:46 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+        switch(x){
+            case 0: 
+                return 0;
+                break;
+            case 1: 
+                return 1;
+                break;
+            default:
+                return fibonacci(x-1) + fibonacci(x-2);
+                break;
+        }
 }

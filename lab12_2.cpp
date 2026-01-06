ec01e76a3f2ced4af9fed0d3ba4e3e502181e468
BiscitAlt <thitiputt.pe@gmail.com>
2026-01-06 20:25:47 +0700

    lab12_2 completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y){
+    return (x % y == 0) ? y : gcd(y, x%y); 
 }
